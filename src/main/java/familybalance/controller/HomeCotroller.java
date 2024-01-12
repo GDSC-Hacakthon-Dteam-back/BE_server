996a6d4a7e710279958c43a92f3294553716755c
abt576 <limut0913@gmail.com>
2024-01-13 00:38:25 +0900

    server

---
+++ src/main/java/familybalance/controller/HomeCotroller.java
@@ -0,0 +1,12 @@
+package familybalance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeCotroller {
+    @GetMapping("/") // 기본 페이지
+    public String index() {
+        return "index";
+    }
+}
